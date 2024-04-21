6c9325d1f308d36db505a2c9b0dc8bbc55e3c5a3
alpsarikisla <50963484+alpsarikisla@users.noreply.github.com>
2024-04-21 18:23:43 +0300

    KatmanEklendi

---
+++ VeriErisimKatmani/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VeriErisimKatmani")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("VeriErisimKatmani")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a8223906-fcda-45a7-8563-ca34cf96309f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VeriErisimKatmani/VeriModeli.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeriErisimKatmani
+{
+    public class VeriModeli
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        public VeriModeli()
+        {
+            baglanti = new SqlConnection(BaglantiYollari.BagnatiYolu);
+            komut = baglanti.CreateCommand();
+        }
+
+        #region Kategori Metotları
+
+        public bool KategoriEkle(Kategori kat)
+        {
+            try
+            {
+                komut.CommandText = "INSERT INTO Kategoriler(Isim, Aciklama, Durum) VALUES(@isim,@aciklama,@durum)";
+                komut.Parameters.Clear();
+                komut.Parameters.AddWithValue("@isim", kat.Isim);
+                komut.Parameters.AddWithValue("@aciklama", kat.Aciklama);
+                komut.Parameters.AddWithValue("@durum", kat.Durum);
+                baglanti.Open();
+                komut.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                baglanti.Close();
+            }
+        }
+
+        #endregion
+    }
+}
